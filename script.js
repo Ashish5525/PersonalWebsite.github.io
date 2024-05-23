e1d3683e91df3b461cc61dab9917cfa485093654
Ashish Gaurav Surapuraju <surapurajua@gmail.com>
2024-05-23 17:39:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 class CssPropControl {
-    constructor(element) {
-      this.element = element
-    }
-    get(varName) {
-      return getComputedStyle(this.element).getPropertyValue(varName)
-    }
-    set(varName, val) {
-      return this.element.style.setProperty(varName, val)
-    }
+  constructor(element) {
+    this.element = element;
   }
-  
-  const bodyCssProps = new CssPropControl(document.body)
-  
-  let toggle = document.querySelector('#dark-mode-toggle')
-  toggle.addEventListener('click', () => { 
-    let mode = toggle.checked ? 'dark' : 'light'
-    bodyCssProps.set('--background', bodyCssProps.get(`--${mode}-background`))
-    bodyCssProps.set('--primary', bodyCssProps.get(`--${mode}-primary`))
-    bodyCssProps.set('--link', bodyCssProps.get(`--${mode}-link`))
-  })
+
+  get(varName) {
+    return getComputedStyle(this.element).getPropertyValue(varName);
+  }
+
+  set(varName, val) {
+    return this.element.style.setProperty(varName, val);
+  }
+}
 
 document.addEventListener('DOMContentLoaded', function() {
-  const dropdownButtons = document.querySelectorAll('.dropdown-btn');
+  const bodyCssProps = new CssPropControl(document.body);
+  let toggle = document.querySelector('#dark-mode-toggle');
 
-  dropdownButtons.forEach(button => {
-    button.addEventListener('click', function() {
-      const dropdownContent = this.nextElementSibling;
+  toggle.addEventListener('click', () => {
+    let mode = toggle.checked ? 'dark' : 'light';
+    bodyCssProps.set('--background', bodyCssProps.get(`--${mode}-background`));
+    bodyCssProps.set('--primary', bodyCssProps.get(`--${mode}-primary`));
+    bodyCssProps.set('--link', bodyCssProps.get(`--${mode}-link`));
+  });
+
+  document.querySelectorAll('.dropdown-btn').forEach(button => {
+    button.addEventListener('click', () => {
+      const dropdownContent = button.nextElementSibling;
+      button.classList.toggle('active');
+      
       if (dropdownContent.classList.contains('show')) {
         dropdownContent.classList.remove('show');
+        dropdownContent.classList.add('hide');
         dropdownContent.style.maxHeight = null;
       } else {
+        dropdownContent.classList.remove('hide');
         dropdownContent.classList.add('show');
         dropdownContent.style.maxHeight = dropdownContent.scrollHeight + 'px';
       }
     });
   });
 });
-
-  
